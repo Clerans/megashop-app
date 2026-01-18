b60513cf1e06ffef437086cd3d700b8836bf8827
Clerans <michaelclerans01@gmail.com>
2026-01-18 22:11:34 +0530

    Components Update

---
+++ Front-end/src/components/BottomNavigation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// This component is currently serving as a placeholder or can be used for custom tab bar implementation.
+// The main logic is currently handled in Navigation/AppNavigator.js using basic Tab.Navigator.
+
+const BottomNavigation = () => {
+    return (
+        <View>
+            <Text>Bottom Navigation Component</Text>
+        </View>
+    );
+};
+
+export default BottomNavigation;
